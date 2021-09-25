c3dda034c7425a7f112d7c5150a056d22af14bee
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-09-25 22:18:52 +0530

    Create find a string.py

---
+++ find a string.py
@@ -0,0 +1,14 @@
+# code:
+def count_substring(string, sub_string):
+    c=0
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            c+=1
+    return c
+
+if __name__ == '__main__':
+  string = raw_input().strip()
+    sub_string = raw_input().strip()
+    
+    count = count_substring(string, sub_string)
+    print count
