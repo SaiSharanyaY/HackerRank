a78c452ad46aafce912a9e56ed647761d33e4ef1
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-09-25 21:27:19 +0530

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -56,3 +56,16 @@ if __name__ == '__main__':
             lis.reverse()
         else:
             print("Invalid Input")
+            
+# another model of code :
+# n = input()
+# l = []
+# for _ in range(n):
+ #   s = raw_input().split()
+ #   cmd = s[0]
+ #   args = s[1:]
+  #  if cmd !="print":
+  #      cmd += "("+ ",".join(args) +")"
+   #     eval("l."+cmd)
+ #  else:
+   #     print l
